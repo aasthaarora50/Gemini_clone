7adcdebe201747ad04eb9caf2a530bcaa0f5303b
aasthaarora50 <89250434+aasthaarora50@users.noreply.github.com>
2024-07-31 11:21:34 +0530

    Add files via upload

---
+++ src/context/Context.jsx
@@ -0,0 +1,50 @@
+import { createContext, useCallback, useState } from "react";
+import run from "../config/Gemini";
+
+export const Context= createContext();
+
+const ContextProvider= (props)=>{
+    const [input, setInput]=useState("");
+    const [recentPrompt, setRecentPrompt]=useState("");
+    const [prevPrompt, setPrevPrompt]=useState([]);
+    const [showResult, setShowResult]=useState(false);
+    const [loading, setLoading]=useState(false);
+    const [resultData, setResultData]=useState("");
+
+
+    const onSent= async (prompt)=>{
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+        setRecentPrompt(input)
+        const response =await run(input)
+        setResultData(response)
+        setLoading(false)
+        setInput("")
+    }
+
+    
+    const contextValue={
+        prevPrompt,
+        setPrevPrompt,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput
+        
+    
+
+    }
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+
+}
+
+export default ContextProvider
